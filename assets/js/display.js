d749a030de8d33db1b7a4dcde9a8188c082d6091
Aditya <ahdithiya@gmail.com>
2022-05-19 15:16:58 +0700

    main

--- assets/js/display.js
+++ assets/js/display.js
@@ -1,7 +1,7 @@
 Vue.createApp({
   data() {
     return {
-      article: null, /
+      article: null, //variabel article
     };
   },
   methods: {
@@ -9,15 +9,15 @@ Vue.createApp({
       const queryString = window.location.search;
       const urlParams = new URLSearchParams(queryString);
       const article = urlParams.get("articles");
-      var converter = new showdown.Converter(); 
+      var converter = new showdown.Converter(); //mendefinisikan konverter
       axios
         .get(
-          "https://raw.githubusercontent.com/ahdithya/tekweb2022/main/data/" +
+          "https://raw.githubusercontent.com/ahdithya/tekweb2022/main/data" +
             article
         )
         .then((res) => {
-          var html = converter.makeHtml(res.data); 
-          this.article = html; 
+          var html = converter.makeHtml(res.data); //eksekusi konverter
+          this.article = html; //update variabel article
         })
         .catch((error) => {
           console.log(error);
