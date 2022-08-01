2878cf7c130bf7ef9ae649796e05dc748ebfa7b3
Aditya Atallah <91508590+ahdithya@users.noreply.github.com>
2022-08-01 20:11:59 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,29 +59,11 @@ Vue.createApp({
           console.log(error);
         });
     },
-    getMarkdown() {
-      const queryString = window.location.search;
-      const urlParams = new URLSearchParams(queryString);
-      const articles = urlParams.get("article");
-      var converter = new showdown.Converter();
-      console.log(articles);
-      axios
-        .get((src = "./data/" + articles))
-        .then((res) => {
-          var html = converter.makeHtml(res.data);
-          this.article = html;
-          console.log(html);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
   },
   beforeMount() {
     this.getHeaderData();
     this.getNavbarData();
     this.getAboutData();
     this.getArticles();
-    this.getMarkdown();
   },
 }).mount("#app");
